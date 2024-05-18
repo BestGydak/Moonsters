97dde40dddfea94d40f1710e5f6c10fd5f122783
BestGydak <115108761+BestGydak@users.noreply.github.com>
2024-05-18 16:11:28 +0500

    Turret

--- Assets/Moonsters/Scripts/Characters/NonPlayable/Turret.cs
+++ Assets/Moonsters/Scripts/Characters/NonPlayable/Turret.cs
@@ -2,17 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Turret : MonoBehaviour
+namespace Moonsters
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Turret : MonoBehaviour
     {
+        [SerializeField] private float minAttackDelay;
+        [SerializeField] private float maxAttackDelay;
+        [SerializeField] private Health healthTarget;
+        [SerializeField] private float maxDistanceToAttack;
+        [Header("Projectile Settings")]
+        [SerializeField] private Projectile projectilePrefab;
+        [SerializeField] private float projectileSpeed;
         
-    }
+        public Health HealthTarget
+        {
+            get => healthTarget;
+            set => healthTarget = value;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Start()
+        {
+            StartCoroutine(AttackCoroutine());
+        }
+
+        private IEnumerator AttackCoroutine()
+        {
+            while(true)
+            {
+                if(Vector2.Distance(healthTarget.transform.position, transform.position) > maxDistanceToAttack)
+                {
+                    yield return new WaitForFixedUpdate();
+                    continue;
+                }
+                var direction = ((Vector2)healthTarget.transform.position - 
+                    (Vector2)transform.position).normalized;
+                var projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+                projectile.LaunchProjectile(direction, projectileSpeed);
+                yield return new WaitForSeconds(Random.Range(minAttackDelay, maxAttackDelay));
+            }
+        }
+
+        public void Die()
+        {
+            Destroy(gameObject);
+        }    
     }
 }
