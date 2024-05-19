6f403ab20f7b1b0d3f20980965cb6130677a9fd7
BestGydak <115108761+BestGydak@users.noreply.github.com>
2024-05-19 18:35:21 +0500

    Fixes

---
+++ Assets/Moonsters/Scripts/MaxAmmoBuff.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Moonsters
+{
+    public class MaxAmmoBuff : MonoBehaviour
+    {
+        [SerializeField] private int buff;
+    }
+}
